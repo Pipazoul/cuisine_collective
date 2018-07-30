6c409064b322ae73274da167e088a7043b6095f9
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2018-07-30 16:03:42 +0200

    add a pin

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,15 +14,6 @@ export class AppComponent implements OnInit, AfterViewInit {
   initialZoom: number = 11;
   map: ol.Map;
   markerSource = new ol.source.Vector();
-  markerStyle = new ol.style.Style({
-    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */({
-      anchor: [0.5, 46],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'pixels',
-      opacity: 0.75,
-      src: 'https://openlayers.org/en/v4.6.4/examples/data/icon.png'
-    }))
-  });
 
   ngAfterViewInit(): void {
     this.map = new ol.Map({
@@ -32,7 +23,6 @@ export class AppComponent implements OnInit, AfterViewInit {
         }),
         new ol.layer.Vector({
           source: this.markerSource,
-          style: this.markerStyle,
         }),
       ],
       target: this.mapElement.nativeElement,
@@ -47,6 +37,25 @@ export class AppComponent implements OnInit, AfterViewInit {
         zoom: this.initialZoom
       })
     });
+    var iconFeatures = [];
+
+    var iconFeature = new ol.Feature({
+      geometry: new ol.geom.Point([538262.3872128094, 5740786.2887582248]),
+      name: 'Null Island',
+      population: 4000,
+      rainfall: 500
+    });
+
+    iconFeature.setStyle(
+      new ol.style.Style({
+        image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+          color: '#8959A8',
+          crossOrigin: 'anonymous',
+          src: 'assets/pin.png',
+          anchor: [0.5, 1]
+        }))
+    }));
+    this.markerSource.addFeature(iconFeature);
   }
 
   ngOnInit(): void {
@@ -56,15 +65,6 @@ export class AppComponent implements OnInit, AfterViewInit {
   addMarker(lon, lat) {
 
 
-    var iconFeatures = [];
-
-    var iconFeature = new ol.Feature({
-      geometry: new ol.geom.Point([lon, lat]),
-      name: 'Null Island',
-      population: 4000,
-      rainfall: 500
-    });
-
-    this.markerSource.addFeature(iconFeature);
+    
   }
 }
