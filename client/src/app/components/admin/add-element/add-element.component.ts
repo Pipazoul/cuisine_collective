1b390d65384ef2f9f062e1b7bba3eba657f9ffd0
VAUTRAIN Christophe <christophe.vautrain@ext.soprasteria.com>
2018-07-30 12:39:22 +0200

    Add (Front-end): using carousel in add element component

--- client/src/app/components/admin/add-element/add-element.component.ts
+++ client/src/app/components/admin/add-element/add-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CarouselComponent } from '../../carousel/carousel.component';
 
 @Component({
   selector: 'app-add-element',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddElementComponent implements OnInit {
 
+  @ViewChild('carousel') public carousel: CarouselComponent;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public showCreateEvent() {
+    this.carousel.goTo(1);
   }
 
 }
