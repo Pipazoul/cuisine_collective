6e2046774a3731fa9a4fe4568047cc10bbf1698c
VAUTRAIN Christophe <christophe.vautrain@ext.soprasteria.com>
2018-08-01 09:58:08 +0200

    Add (Both): missing properties on event entity to save location & type

--- client/src/app/domain/event.class.ts
+++ client/src/app/domain/event.class.ts
@@ -17,6 +17,19 @@ export class EventClass {
     public longitude: number;
     public latitude: number;
     public occurenceType: OccurenceType;
+    public locationLabel: string;
+    public informations: string;
+    public eat: boolean = false;
+    public cook: boolean = false;
+    public public: boolean = false;
+    // All about location from API
+    public locationCity: string;
+    public locationCitycode: string;
+    public locationHousenumber: string;
+    public locationName: string;
+    public locationPostcode: string;
+    public locationStreet: string;
+
     constructor(obj?: any) {
         Object.assign(this, obj);
     }
