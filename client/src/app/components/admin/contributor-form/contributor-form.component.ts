9182f0c73911114de4c4fc1629af3300fbfaa497
jc950 <luquet.jc@gmail.com>
2018-08-01 11:20:18 +0200

    Add (Front-end): contributor address

--- client/src/app/components/admin/contributor-form/contributor-form.component.ts
+++ client/src/app/components/admin/contributor-form/contributor-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { filter, debounceTime, switchMap } from 'rxjs/operators';
+import { MatAutocompleteSelectedEvent } from '@angular/material';
+import * as ol from 'openlayers';
+import * as _ from 'lodash';
 
 // Utils
 import { CustomRegExp } from '../../../util/CustomRegExp';
@@ -7,9 +11,11 @@ import { AbstractContributorModifier } from '../../../abstract/abstract-contribu
 
 // Services
 import { ContributorService } from '../../../services/contributor.service';
+import { LocationService } from '../../../services/location.service';
 
 // Classes
 import { ContributorClass } from '../../../domain/contributor.class';
+import { LocationClass } from '../../../domain/location.class';
 
 @Component({
   selector: 'app-contributor-form',
@@ -18,30 +24,66 @@ import { ContributorClass } from '../../../domain/contributor.class';
 })
 export class ContributorFormComponent extends AbstractContributorModifier implements OnInit {
 
-  public contributorForm: FormGroup;
+  private static readonly RESULTS_LIMIT = 5;
 
-  constructor(@Inject(ContributorService) contributorService: ContributorService) {
+  public contributorForm: FormGroup;
+  public locations: LocationClass[] = [];
+  public isLoading: boolean = false;
+
+  constructor(
+    @Inject(ContributorService) contributorService: ContributorService,
+    private locationService: LocationService) {
     super(contributorService);
   }
 
   ngOnInit() {
-    this.initForm();
-  }
+    const locationCtrl = new FormControl();
+    locationCtrl.valueChanges.pipe(
+      filter(data => {
+        this.locations.length = 0;
+        return _.isString(data) && (data.trim().length ? true : false) && (this.isLoading = true)
+      }),
+      debounceTime(500),
+      // Using switchMap to prevent displaying data returned if another request is in progress
+      switchMap(data => this.locationService.search(data, ContributorFormComponent.RESULTS_LIMIT)))
+      .subscribe(data => {
+        this.isLoading = false;
+        this.locations = data.features;
+      });
 
-  private initForm() {
     this.contributorForm = new FormGroup({
       'title': new FormControl(this.contributor.title, Validators.required),
       'description': new FormControl(this.contributor.description),
       'name': new FormControl(this.contributor.name),
+      'location': locationCtrl,
       'hours': new FormControl(this.contributor.hours),
       'email': new FormControl(this.contributor.email, Validators.email),
-      'phone': new FormControl(this.contributor.phone, Validators.pattern(CustomRegExp.PHONE)),
-      'address': new FormControl(this.contributor.address),
+      'phone': new FormControl(this.contributor.phone, Validators.pattern(CustomRegExp.PHONE))
+    });
+  }
+
+  /**
+   * When user select a result among the list
+   * 
+   * @param event Clicked result
+   */
+  public onLocationSelected(event: MatAutocompleteSelectedEvent) {
+    const location: LocationClass = event.option.value;
+    this.contributorForm.get('location').setValue(location.properties.label);
+    const transformedLocation = ol.proj.fromLonLat(location.geometry.coordinates, 'EPSG:3857');
+    Object.assign(this.contributor, {
+      longitude: transformedLocation[0],
+      latitude: transformedLocation[1],
+      houseNumber: location.properties.housenumber,
+      street: location.properties.street,
+      zipcode: location.properties.citycode,
+      city: location.properties.city
     });
   }
 
   public submitForm(value) {
-    this.saveContributor(new ContributorClass(value));
+    Object.assign(this.contributor, value);
+    this.saveContributor(this.contributor);
   }
 
 }
--- client/src/app/domain/contributor.class.ts
+++ client/src/app/domain/contributor.class.ts
@@ -7,10 +7,6 @@ export class ContributorClass {
     public hours: string;
     public email: string;
     public phone: string;
-    public houseNumber: string;
-    public address: string;
-    public zipcode: string;
-    public city: string;
     public longitude: number;
     public latitude: number;
     public location: boolean;
@@ -18,6 +14,11 @@ export class ContributorClass {
     public skills: boolean;
     public people: boolean;
     public assistants: boolean;
+    // All about location from API
+    public houseNumber: string;
+    public street: string;
+    public zipcode: string;
+    public city: string;
 
     constructor(obj?: any) {
         Object.assign(this, obj);
