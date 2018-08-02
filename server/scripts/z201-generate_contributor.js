52e8d4c88e0524435f861fa83d5a489ab45bd694
jc950 <luquet.jc@gmail.com>
2018-08-02 18:32:35 +0200

    Fix (Back-end): contributors generation

--- server/scripts/z201-generate_contributor.js
+++ server/scripts/z201-generate_contributor.js
@@ -11,8 +11,8 @@ module.exports = {
       'description': `Les bénévoles des petits frères de pauvres peuvent vous accompagner dans les ateliers cuisine ou les repas collectifs près de chez vous.
         De porte à porte, dans les transports en commun, ou durant tout l\'atelier, nous conviendrons ensemble du format qui vous convient le mieux.`,
       'name': 'Marie DUPONT',
-      'hours': `Lundi: 12h00-14h00\r\nMardi : 12h00-14h00\r\nMercredi : 12h00-14h00 / 19h00-21h00
-        Jeudi :12h00-14h00 / 19h00-21 h00\r\nVendredi : 12h00-14h00 / 19h00-21h00\r\nSamedi : Fermé\r\nDimanche : Fermé`,
+      'hours': 'Lundi: 12h00-14h00\r\nMardi : 12h00-14h00\r\nMercredi : 12h00-14h00 / 19h00-21h00\r\n' +
+        'Jeudi :12h00-14h00 / 19h00-21 h00\r\nVendredi : 12h00-14h00 / 19h00-21h00\r\nSamedi : Fermé\r\nDimanche : Fermé',
       'email': 'mdupont@grandyon.com',
       'phone': '0426647924',
       'houseNumber': '8',
@@ -30,8 +30,8 @@ module.exports = {
       'title': 'Office of Strategic Services',
       'description': 'Distribution gratuite de biscottes et de beurre',
       'name': 'Jean DUJARDIN',
-      'hours': `Lundi: 12h00-14h00\r\nMardi : 12h00-14h00\r\nMercredi : 12h00-14h00 / 19h00-21h00
-        Jeudi :12h00-14h00 / 19h00-21 h00\r\nVendredi : 12h00-14h00 / 19h00-21h00\r\nSamedi : Fermé\r\nDimanche : Fermé`,
+      'hours': 'Lundi: 12h00-14h00\r\nMardi : 12h00-14h00\r\nMercredi : 12h00-14h00 / 19h00-21h00\r\n' +
+        'Jeudi :12h00-14h00 / 19h00-21 h00\r\nVendredi : 12h00-14h00 / 19h00-21h00\r\nSamedi : Fermé\r\nDimanche : Fermé',
       'email': 'jdujardin@grandyon.com',
       'phone': '0426647924',
       'houseNumber': '8',
@@ -49,8 +49,8 @@ module.exports = {
       'title': 'Confédération paysanne',
       'description': 'Distribution gratuite de maïs sans OGM',
       'name': 'José BOVE',
-      'hours': `Lundi: 12h00-14h00\r\nMardi : 12h00-14h00\r\nMercredi : 12h00-14h00 / 19h00-21h00
-        Jeudi :12h00-14h00 / 19h00-21 h00\r\nVendredi : 12h00-14h00 / 19h00-21h00\r\nSamedi : Fermé\r\nDimanche : Fermé`,
+      'hours': 'Lundi: 12h00-14h00\r\nMardi : 12h00-14h00\r\nMercredi : 12h00-14h00 / 19h00-21h00\r\n' +
+        'Jeudi :12h00-14h00 / 19h00-21 h00\r\nVendredi : 12h00-14h00 / 19h00-21h00\r\nSamedi : Fermé\r\nDimanche : Fermé',
       'email': 'jbove@grandyon.com',
       'phone': '0426647924',
       'houseNumber': '8',
@@ -69,8 +69,8 @@ module.exports = {
       'description': `Aider et apporter une assistance bénévole aux personnes démunies, notamment dans le domaine alimentaire par l'accès à des repas gratuits,
         et par la participation à leur insertion sociale et économique, ainsi qu'à toute l'action contre la pauvreté sous toutes ses formes`,
       'name': 'Coluche',
-      'hours': `Lundi: 12h00-14h00\r\nMardi : 12h00-14h00\r\nMercredi : 12h00-14h00 / 19h00-21h00
-        Jeudi :12h00-14h00 / 19h00-21 h00\r\nVendredi : 12h00-14h00 / 19h00-21h00\r\nSamedi : Fermé\r\nDimanche : Fermé`,
+      'hours': 'Lundi: 12h00-14h00\r\nMardi : 12h00-14h00\r\nMercredi : 12h00-14h00 / 19h00-21h00\r\n' +
+        'Jeudi :12h00-14h00 / 19h00-21 h00\r\nVendredi : 12h00-14h00 / 19h00-21h00\r\nSamedi : Fermé\r\nDimanche : Fermé',
       'email': 'restos-du-coeur@grandyon.com',
       'phone': '0426647924',
       'houseNumber': '8',
@@ -88,8 +88,8 @@ module.exports = {
       'title': 'Cauchemard en cuisine',
       'description': 'Offrir à tous la possibilité de s\'améliorer en cuisinant, grâce à l\'intervention d\'un coach expérimenté',
       'name': 'Philippe Etchebest',
-      'hours': `Lundi: 12h00-14h00\r\nMardi : 12h00-14h00\r\nMercredi : 12h00-14h00 / 19h00-21h00
-        Jeudi :12h00-14h00 / 19h00-21 h00\r\nVendredi : 12h00-14h00 / 19h00-21h00\r\nSamedi : Fermé\r\nDimanche : Fermé`,
+      'hours': 'Lundi: 12h00-14h00\r\nMardi : 12h00-14h00\r\nMercredi : 12h00-14h00 / 19h00-21h00\r\n' +
+        'Jeudi :12h00-14h00 / 19h00-21 h00\r\nVendredi : 12h00-14h00 / 19h00-21h00\r\nSamedi : Fermé\r\nDimanche : Fermé',
       'email': 'philippe-etchebest@grandyon.com',
       'phone': '0426647924',
       'houseNumber': '8',
