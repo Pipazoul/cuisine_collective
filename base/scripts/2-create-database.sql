eee77af4ba25a942e79b8a480359e501ce8c9e73
jc950 <luquet.jc@gmail.com>
2018-08-21 15:35:38 +0200

    Add docker

---
+++ base/scripts/2-create-database.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE cuisine_collective_dev
+    WITH 
+    OWNER = cuisine_collective_dev
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
---
+++ base/scripts/3-create_schema.sql
@@ -0,0 +1,4 @@
+\connect cuisine_collective_dev;
+DROP SCHEMA IF EXISTS cuisine_collective_dev CASCADE;
+CREATE SCHEMA cuisine_collective_dev
+    AUTHORIZATION cuisine_collective_dev;
