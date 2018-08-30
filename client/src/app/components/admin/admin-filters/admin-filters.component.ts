8bcaccd3e994e4a7fad357e0c4117cce2547a14a
jc950 <luquet.jc@gmail.com>
2018-08-30 17:00:43 +0200

    Add (Front-end): hide public filed

--- client/src/app/components/admin/admin-filters/admin-filters.component.ts
+++ client/src/app/components/admin/admin-filters/admin-filters.component.ts
@@ -17,7 +17,7 @@ export class AdminFiltersComponent implements OnInit {
   public endDate: Date;
   public eatToggle: boolean = false;
   public cookToggle: boolean = false;
-  public publicToggle: boolean = false;
+  /* public publicToggle: boolean = false; */
   public regularToggle: boolean = false;
   public missingLocation: boolean = false;
   public missingFood: boolean = false;
@@ -51,7 +51,7 @@ export class AdminFiltersComponent implements OnInit {
     this.filterEvents.emit({
       eat: this.eatToggle,
       cook: this.cookToggle,
-      public: this.publicToggle,
+      /* public: this.publicToggle, */
       regular: this.regularToggle,
       startDate: this.startDate,
       endDate: this.endDate,
