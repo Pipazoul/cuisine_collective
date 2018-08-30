62cc15f9643339f26c1c209760c960ab8f4afec5
jc950 <luquet.jc@gmail.com>
2018-08-30 17:35:50 +0200

    Add (Front & Back): add inscription field

--- client/src/app/domain/event.class.ts
+++ client/src/app/domain/event.class.ts
@@ -23,6 +23,7 @@ export class EventClass {
     public partnerInformations: string;
     public eat: boolean = false;
     public cook: boolean = false;
+    public inscription: boolean = false;
     /* public public: boolean = false; */
     // All about location from API
     public locationCity: string;
