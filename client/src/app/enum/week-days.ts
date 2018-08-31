81822f9e40aab74d5ba17a6ec3a92ee305fa32fa
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2018-08-31 18:11:11 +0200

    replace week days magic strings with Symbols fix forms bugs

---
+++ client/src/app/enum/week-days.ts
@@ -0,0 +1,9 @@
+export class WeekDays {
+    static readonly MONDAY = Symbol();
+    static readonly TUESDAY = Symbol();
+    static readonly WEDNESDAY = Symbol();
+    static readonly THURSDAY = Symbol();
+    static readonly FRIDAY = Symbol();
+    static readonly SATURDAY = Symbol();
+    static readonly SUNDAY = Symbol();
+}
