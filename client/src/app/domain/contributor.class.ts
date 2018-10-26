ebcaeef9814a43cff42e6c1f892d9c128ed9a480
VAUTRAIN Christophe <christophe.vautrain@ext.soprasteria.com>
2018-10-26 17:16:57 +0200

    Fix (Both): contributors mandatory fields

--- client/src/app/domain/contributor.class.ts
+++ client/src/app/domain/contributor.class.ts
@@ -9,11 +9,11 @@ export class ContributorClass {
     public phone: string;
     public longitude: number;
     public latitude: number;
-    public location: boolean;
-    public food: boolean;
-    public skills: boolean;
-    public people: boolean;
-    public assistants: boolean;
+    public location: boolean = false;
+    public food: boolean = false;
+    public skills: boolean = false;
+    public people: boolean = false;
+    public assistants: boolean = false;
     // All about location from API
     public houseNumber: string;
     public street: string;
@@ -21,13 +21,13 @@ export class ContributorClass {
     public city: string;
 
     get formattedAddress() {
-        if(this.street && this.zipcode && this.city) {
+        if (this.street && this.zipcode && this.city) {
             return (this.houseNumber ? this.houseNumber + ' ' : '')
-            + this.street
-            + ' '
-            + this.zipcode
-            + ' '
-            + this.city;
+                + this.street
+                + ' '
+                + this.zipcode
+                + ' '
+                + this.city;
         }
     }
 
