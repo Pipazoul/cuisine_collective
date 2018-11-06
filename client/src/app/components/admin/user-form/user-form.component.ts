46c05977b06b881ae3ddeb52e830ac2c7464a2b6
VAUTRAIN Christophe <christophe.vautrain@ext.soprasteria.com>
2018-11-06 16:27:12 +0100

    Add (Front-end): user form not valid if duplicate email

--- client/src/app/components/admin/user-form/user-form.component.ts
+++ client/src/app/components/admin/user-form/user-form.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { UserClass } from 'src/app/domain/user.class';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as _ from 'lodash';
+
 import { UserService } from 'src/app/services/user.service';
+
+import { UserClass } from 'src/app/domain/user.class';
 
 @Component({
   selector: 'app-user-form',
@@ -31,9 +34,22 @@ export class UserFormComponent implements OnInit {
   public submitForm(value) {
     Object.assign(this.user, value);
     if (this.user.id) {
-      this.userService.update(this.user).subscribe(() => this.router.navigate(['/admin', 'users']));
+      this.userService.update(this.user).subscribe(
+        () => this.router.navigate(['/admin', 'users']),
+        (err) => this.handleError(err));
     } else {
-      this.userService.create(this.user).subscribe(() => this.router.navigate(['/admin', 'users']));
+      this.userService.create(this.user).subscribe(
+        () => this.router.navigate(['/admin', 'users']),
+        (err) => this.handleError(err));
+    }
+  }
+
+  private handleError(err) {
+    if (err.error.error.statusCode === 422 && err.error.error.details && err.error.error.details.codes && err.error.error.details.codes.email && _.includes(err.error.error.details.codes.email, 'uniqueness')) {
+      this.userForm.get('email').setErrors({ 'emailAlreadyExists': true });
+    } else {
+      // TODO message ?
+      console.error(err);
     }
   }
 
