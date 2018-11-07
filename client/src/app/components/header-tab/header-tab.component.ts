142d15ee7f45989b12b2725ea10f623d7bd3082e
VAUTRAIN Christophe <christophe.vautrain@ext.soprasteria.com>
2018-11-07 17:27:53 +0100

    Add (Front-end): navigate to /admin when clicking on header tab

--- client/src/app/components/header-tab/header-tab.component.ts
+++ client/src/app/components/header-tab/header-tab.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { HeaderTabService } from 'src/app/services/header-tab.service';
 import { TabSelectionType } from 'src/app/enum/tab-selection-type.enum';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-tab',
@@ -13,7 +14,8 @@ export class HeaderTabComponent implements OnInit {
 
   public selectionType: TabSelectionType = HeaderTabService.DEFAULT_TYPE;
 
-  constructor(private headerTabService: HeaderTabService) {
+  constructor(private router: Router,
+    private headerTabService: HeaderTabService) {
 
   }
 
@@ -22,12 +24,14 @@ export class HeaderTabComponent implements OnInit {
 
   public selectEvents() {
     if (this.selectionType !== TabSelectionType.EVENTS) {
+      this.router.navigate(['/admin']);
       this.headerTabService.typeChanged.next(this.selectionType = TabSelectionType.EVENTS);
     }
   }
 
   public selectContributors() {
     if (this.selectionType !== TabSelectionType.CONTRIBUTORS) {
+      this.router.navigate(['/admin']);
       this.headerTabService.typeChanged.next(this.selectionType = TabSelectionType.CONTRIBUTORS);
     }
   }
